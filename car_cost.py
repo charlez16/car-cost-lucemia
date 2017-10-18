9168b5d614d4a5f3b188c45d48716a4ee151d16c
Chuckk16 <chuckthebuck16@gmail.com>
2017-10-18 15:13:35 -0400

    car cost project

--- car_cost.py
+++ car_cost.py
@@ -8,3 +8,7 @@
 carValue = float(input("What is the value of your current car: "))
 registrationFee = 192
 dealerFee = 200
+deliveryFee = 120
+extras = registrationFee + dealerFee + deliveryFee
+total = extras + carValue
+payment = ((0.09(1 + 0.09)**36)/(((1 + 0.09)**36)-1))carValue
