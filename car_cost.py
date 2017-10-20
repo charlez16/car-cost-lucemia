a4b1ab66b6a270361bde1f01bf923f43d06fcdc9
Chuckk16 <chuckthebuck16@gmail.com>
2017-10-20 14:56:16 -0400

    car cost project

--- car_cost.py
+++ car_cost.py
@@ -1,14 +1,20 @@
+#car_cost
+#The problem is to find out how much the car is going to be monthly
+#Charles & Enes
+#10/20/17
+#There are no bugs
 
-
-
-
-
-
-
-carValue = float(input("What is the value of your current car: "))
+carValue = float(input("What is the value of your new car: "))
+salesTax = 0.06 * carValue
 registrationFee = 192
 dealerFee = 200
 deliveryFee = 120
 extras = registrationFee + dealerFee + deliveryFee
-total = extras + carValue
-payment = ((0.09(1 + 0.09)**36)/(((1 + 0.09)**36)-1))carValue
+total = salesTax + extras + carValue
+r = 0.09 / 12
+payment = ((r*(1 + r)**36) \
+           /(((1 + r)**36)-1))*carValue
+print ("total cost "+ str(total))
+print ("payment " + str(int(payment)))
+
+
